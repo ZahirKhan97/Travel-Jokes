8afe111092db6d808049af295db2d28922b2b242
MZK <139211996+ZahirKhan97@users.noreply.github.com>
2025-09-24 23:01:48 +0500

    Design Password Change Page on User Panel

--- js/main.js
+++ js/main.js
@@ -89,6 +89,21 @@
             }
         }
     });
+
+  document.querySelectorAll(".toggle-password").forEach((icon) => {
+    icon.addEventListener("click", function () {
+      const input = document.getElementById(this.dataset.target);
+      if (input.type === "password") {
+        input.type = "text";
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+      } else {
+        input.type = "password";
+        this.classList.remove("fa-eye-slash");
+        this.classList.add("fa-eye");
+      }
+    });
+  });
     
 })(jQuery);
 
